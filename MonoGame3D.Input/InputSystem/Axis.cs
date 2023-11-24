5725644159f2bf2fc6784dcc4621db66e9e4deeb
nemodesm <nemodesm@gmail.com>
2023-11-24 15:11:50 +0100

    chore: Removed antiquated AxisDefinition

--- MonoGame3D.Input/InputSystem/Axis.cs
+++ MonoGame3D.Input/InputSystem/Axis.cs
@@ -4,7 +4,14 @@ using MonoGame3D.InputSystem.Legacy;
 
 namespace MonoGame3D.InputSystem;
 
-public readonly record struct Axis(Keys[] negative, Keys[] positive, int bothValue, float deadzone)
+public readonly record struct Axis(Keys[] negative, Keys[] positive, int bothValue = 0, float deadzone = 0.05f)
 {
-    public float Value => negative.Any(Input.GetKey) ? positive.Any(Input.GetKey) ? bothValue : -1 : positive.Any(Input.GetKey) ? 1 : 0;
+    public float Value
+    {
+        get
+        {
+            return negative.Any(Input.GetKey) ? positive.Any(Input.GetKey) ? bothValue : -1 :
+                positive.Any(Input.GetKey) ? 1 : 0;
+        }
+    }
 }
--- MonoGame3D.Input/InputSystem/Binding.cs
+++ MonoGame3D.Input/InputSystem/Binding.cs
@@ -11,11 +11,11 @@ public readonly record struct Binding(BindingType Type, Keys[]? Keys = default, 
         switch (Type)
         {
             case BindingType.Button:
-                return Keys.Any(Input.GetKey);
+                return Keys!.Any(Input.GetKey);
             case BindingType.Axis:
-                return horizontal.Value.Value != 0;
+                return horizontal!.Value.Value != 0;
             case BindingType.Axis2D:
-                return horizontal.Value.Value != 0 || vertical.Value.Value != 0;
+                return horizontal!.Value.Value != 0 || vertical!.Value.Value != 0;
             default:
                 throw new ArgumentOutOfRangeException(nameof(Type), Type, null);
         }
